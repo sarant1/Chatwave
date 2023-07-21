c2c9a6e4d8b95947a7f95a54af764819cca0a1bd
sarant1 <samuel.arant10@gmail.com>
2023-07-21 14:14:14 -0400

    feat(ddb): add dynamodb scripts for adding cognito users to database

---
+++ bin/cognito-update-dynamodb.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import boto3
+
+client = boto3.client('cognito-idp')
+
+response = client.list_users(
+    UserPoolId='us-east-1_C030yxgjN',
+    AttributesToGet=[
+        'email',
+    ],
+    Limit=10,
+)
+users = response['Users']
+ddb = boto3.client('dynamodb')
+for user in users:
+    uuid = user['Username']
+    email = user['Attributes'][0]['Value']
+    ddb.put_item(
+        TableName='ChatWave',
+        Item={
+            'pk': { 'S': uuid },
+            'sk': { 'S': "Attributes" },
+            'gsiPk': { 'S': email}
+        }
+    )

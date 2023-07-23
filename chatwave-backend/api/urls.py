265e183d9ea73729c782c3b619af9f098fe911ae
sarant1 <samuel.arant10@gmail.com>
2023-07-23 10:36:13 -0400

    feat: remove django backend (replaced with appsync api)

--- chatwave-backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('user', views.user, name='user'),
-    path('room', views.room, name='createroom'),
-    path('messages', views.messages, name='messages')
-]
--- chatwave-backend/formatters/date_time.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def get_room_time(date_string):
-    date_obj = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
-    formatted_date_string = datetime.strftime(date_obj, "%I:%M%p")
-    return formatted_date_string

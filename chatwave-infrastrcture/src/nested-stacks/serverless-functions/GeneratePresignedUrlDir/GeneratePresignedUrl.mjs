62f3d83b19c1e2ad851756f3f0689389f9a1a4a9
sarant1 <samuel.arant10@gmail.com>
2023-07-27 10:51:27 -0400

    feat(photoupload): add fields so photos show in browser

--- chatwave-infrastrcture/src/nested-stacks/serverless-functions/GeneratePresignedUrlDir/GeneratePresignedUrl.mjs
+++ chatwave-infrastrcture/src/nested-stacks/serverless-functions/GeneratePresignedUrlDir/GeneratePresignedUrl.mjs
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { randomUUID } from "crypto";
 import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
 import { S3Client } from "@aws-sdk/client-s3";
@@ -8,11 +7,14 @@ export async function handler(event) {
   const client = new S3Client({ region: "us-east-1" });
   const bucket = "assets.chatwave.com";
   const key = randomUUID() + "." + event.headers.type;
+  const contentType = {
+    "Content-Type": "image/jpeg",
+  };
   const { url, fields } = await createPresignedPost(client, {
     Bucket: bucket,
     Key: key,
+    Fields: contentType,
     Expires: 60 * 5, //Seconds before the presigned post expires. 3600 by default.
-    ContentDisposition: "inline",
   });
   return {
     statusCode: 200,
